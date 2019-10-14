95286cf56c658591616d7448c45a25c03602d10e
League Student <league@iMac-24.attlocal.net>
2019-10-13 17:20:55 -0700

    Day1 Done

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,7 +1,52 @@
 package intro_to_array_lists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Spliterator;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+
+public class GuestBook implements ActionListener{
+	 // Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton("Add Name");
+	JButton button2 = new JButton("View Names");
+    ArrayList<String> names = new ArrayList<String>();
+
+    int num = 5;
+	public static void main(String[] args) {
+		new GuestBook();
+	}
+	public GuestBook() {
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.pack();
+		
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		
+		String listOfName = "Guest #1: Bob Banders \n" + 
+				"Guest #2: Sandy Summers \n" + 
+				"Guest #3: Greg Ganders \n" + 
+				"Guest #4: Donny Doners";
+		String SplitListOfNames[] = listOfName.split("\n");
+		for (int i = 0; i < SplitListOfNames.length; i++) {
+			names.add(SplitListOfNames[i]);
+			
+		}
+		
+
+	}
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
@@ -9,4 +54,17 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == button1) {
+			names.add("Guest #"+num+": Bill Doners");
+			num++;
+		}else {
+			for (int i = 0; i < names.size(); i++) {
+				System.out.println(names.get(i));
+			}
+		}
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,11 +1,34 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> list = new ArrayList<String>();
+
 		//2. Add five Strings to your list
+		for (int i = 0; i < 5; i++) {
+			list.add(i+"");
+			
+			System.out.println(list.get(i));
+			if(i % 2  == 0) {
+				System.out.println(list.get(i));
+				
+			}
+		}
+		for (int i = 5; i > 0; i--) {
+			list.add(i+"");
+			if(list.get(i).contains("e")) {
+				System.out.println(i);
+			}
+			
+		}
+		
+		for (String i : list) {
+			System.out.println(i);
+
+		}
 		
 		//3. Print all the Strings using a standard for-loop
 		
